8e4bcf0a465d8d26fc2cbc32d77c386aa96b5fe7
Alan Denison <alan79td@gmail.com>
2016-08-16 15:04:08 -0700

    add meal component

---
+++ app/meal.component.ts
@@ -0,0 +1,18 @@
+import { Component } from 'angular2/core';
+import { Meal } from './meal.model';
+
+@Component({
+  selector: 'meal-display',
+  inputs: ['meal'],
+  template: `
+  <div>
+    <label> {{ meal.name }}</label>
+    <label>, {{ meal.description }}</label>
+    <label>, {{ meal.calorie }} </label>
+  </div>
+`
+})
+
+export class MealComponent {
+  public meal: Meal;
+}
